80aa4ab4bc80476d50ed57eff1b826f1c85e2241
cdelira9 <cdelira@ucsd.edu>
2022-07-11 08:58:48 -0700

    sleep tab

--- frontend/vite-project/src/components/Sleep/Sleep.jsx
+++ frontend/vite-project/src/components/Sleep/Sleep.jsx
@@ -4,8 +4,26 @@ import "./Sleep.css";
 
 export default function Sleep() {
   return (
-    <div className="sleep">
+    <div className="SleepPage">
       <Navbar />
+      <div className="Banner">
+        <h1>Sleep</h1>
+      </div>
+      <div className="content">
+        <div className="SleepOverview">
+          <div className="header">
+            <h3>Overview</h3>
+            <button className="Button outline small outline blue">
+              Add Sleep
+            </button>
+          </div>
+          <div className="feed">
+            <div className="empty">
+              <h2>Nothing here yet.</h2>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
